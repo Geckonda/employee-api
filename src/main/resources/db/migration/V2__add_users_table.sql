6ee80e89676e3ab59938284b934f485fa86575ac
Geckonda <aquariusstd@yandex.ru>
2026-03-15 23:07:11 +0400

    feat(Migration): изменил миграции

--- src/main/resources/db/migration/V2__add_users_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-                           id UUID PRIMARY KEY,
-                           email VARCHAR(150) UNIQUE NOT NULL,
-                           password VARCHAR(150)  NOT NULL,
-                           role VARCHAR(50) NOT NULL
-)
